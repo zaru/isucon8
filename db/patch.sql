b7409b83d813d29ac2438d47bf553d68dfd86a48
zaru <zarutofu@gmail.com>
2020-08-01 17:26:34 +0900

    add indexes

--- db/patch.sql
+++ db/patch.sql
@@ -9,3 +9,5 @@ create index idx_user_id_event_id on reservations(user_id, event_id);
 alter table reservations add canceled tinyint(1) not null default 0;
 update reservations set canceled = case when canceled_at is not null then 1 else 0 end;
 create index idx_event_id_canceled on reservations(event_id, canceled);
+create index idx_user_id_canceled on reservations(user_id, canceled);
+create index idx_sheet_id_canceled on reservations(sheet_id, canceled);

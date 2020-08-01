3be6b2436bad40c2fdc59b322d8a9dbe8d017beb
zaru <zarutofu@gmail.com>
2020-08-01 14:00:08 +0900

    clean logs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# ログを綺麗にしておく
+echo "" > /var/log/nginx/access.log
+echo "" > /var/log/nginx/error.log
+echo "" > /var/log/mysql/mysql-slow.sql
+
+# アプリケーションを最新にして再起動する
 cd /home/centos/isucon8-qualify/webapp/isucon8
 git pull origin master
 ps aux | grep puma | grep -v grep | awk '{ print "kill -9", $2 }' | sh

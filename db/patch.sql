d601a69e070a293c3eadc699c0eb1267fec907c3
zaru <zarutofu@gmail.com>
2020-08-01 17:21:03 +0900

    add canceled column

--- db/patch.sql
+++ db/patch.sql
@@ -4,3 +4,8 @@ alter table reservations add updated_at datetime(6) null;
 update reservations set updated_at = IFNULL(canceled_at, reserved_at);
 create index idx_updated_at on reservations(updated_at);
 create index idx_user_id_event_id on reservations(user_id, event_id);
+
+-- キャンセル済みフィールドを追加する
+alter table reservations add canceled tinyint(1) not null default 0;
+update reservations set canceled = case when canceled_at is not null then 1 else 0 end;
+create index idx_event_id_canceled on reservations(event_id, canceled);
